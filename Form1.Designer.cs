55652c7ee8441f4a168691228f966fc74f69b842
Beyza <112386181+Kirtan51@users.noreply.github.com>
2023-03-17 14:17:10 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace cafesipariş
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button7sahlepeksi = new System.Windows.Forms.Button();
+            this.button8sahlepartı = new System.Windows.Forms.Button();
+            this.label7sahleps = new System.Windows.Forms.Label();
+            this.label8sahlep = new System.Windows.Forms.Label();
+            this.button5kahveeksi = new System.Windows.Forms.Button();
+            this.button6kahveartı = new System.Windows.Forms.Button();
+            this.label5kahves = new System.Windows.Forms.Label();
+            this.label6kahve = new System.Windows.Forms.Label();
+            this.button3kolaeksi = new System.Windows.Forms.Button();
+            this.button4kolaartı = new System.Windows.Forms.Button();
+            this.label3kolas = new System.Windows.Forms.Label();
+            this.label4kola = new System.Windows.Forms.Label();
+            this.button2çayeksi = new System.Windows.Forms.Button();
+            this.button1çayartı = new System.Windows.Forms.Button();
+            this.label2çays = new System.Windows.Forms.Label();
+            this.label1ÇAY = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightSalmon;
+            this.groupBox1.Controls.Add(this.button7sahlepeksi);
+            this.groupBox1.Controls.Add(this.button8sahlepartı);
+            this.groupBox1.Controls.Add(this.label7sahleps);
+            this.groupBox1.Controls.Add(this.label8sahlep);
+            this.groupBox1.Controls.Add(this.button5kahveeksi);
+            this.groupBox1.Controls.Add(this.button6kahveartı);
+            this.groupBox1.Controls.Add(this.label5kahves);
+            this.groupBox1.Controls.Add(this.label6kahve);
+            this.groupBox1.Controls.Add(this.button3kolaeksi);
+            this.groupBox1.Controls.Add(this.button4kolaartı);
+            this.groupBox1.Controls.Add(this.label3kolas);
+            this.groupBox1.Controls.Add(this.label4kola);
+            this.groupBox1.Controls.Add(this.button2çayeksi);
+            this.groupBox1.Controls.Add(this.button1çayartı);
+            this.groupBox1.Controls.Add(this.label2çays);
+            this.groupBox1.Controls.Add(this.label1ÇAY);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 227);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SİPARİŞ";
+            // 
+            // button7sahlepeksi
+            // 
+            this.button7sahlepeksi.BackColor = System.Drawing.Color.Gainsboro;
+            this.button7sahlepeksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7sahlepeksi.Location = new System.Drawing.Point(244, 174);
+            this.button7sahlepeksi.Name = "button7sahlepeksi";
+            this.button7sahlepeksi.Size = new System.Drawing.Size(51, 42);
+            this.button7sahlepeksi.TabIndex = 15;
+            this.button7sahlepeksi.Text = "-";
+            this.button7sahlepeksi.UseVisualStyleBackColor = false;
+            this.button7sahlepeksi.Click += new System.EventHandler(this.button7sahlepeksi_Click);
+            // 
+            // button8sahlepartı
+            // 
+            this.button8sahlepartı.BackColor = System.Drawing.Color.Silver;
+            this.button8sahlepartı.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8sahlepartı.Location = new System.Drawing.Point(187, 174);
+            this.button8sahlepartı.Name = "button8sahlepartı";
+            this.button8sahlepartı.Size = new System.Drawing.Size(51, 42);
+            this.button8sahlepartı.TabIndex = 14;
+            this.button8sahlepartı.Text = "+";
+            this.button8sahlepartı.UseVisualStyleBackColor = false;
+            this.button8sahlepartı.Click += new System.EventHandler(this.button8sahlepartı_Click);
+            // 
+            // label7sahleps
+            // 
+            this.label7sahleps.AutoSize = true;
+            this.label7sahleps.Location = new System.Drawing.Point(127, 187);
+            this.label7sahleps.Name = "label7sahleps";
+            this.label7sahleps.Size = new System.Drawing.Size(14, 16);
+            this.label7sahleps.TabIndex = 13;
+            this.label7sahleps.Text = "0";
+            this.label7sahleps.Click += new System.EventHandler(this.label7sahleps_Click);
+            // 
+            // label8sahlep
+            // 
+            this.label8sahlep.AutoSize = true;
+            this.label8sahlep.Location = new System.Drawing.Point(43, 187);
+            this.label8sahlep.Name = "label8sahlep";
+            this.label8sahlep.Size = new System.Drawing.Size(60, 16);
+            this.label8sahlep.TabIndex = 12;
+            this.label8sahlep.Text = "SAHLEP";
+            this.label8sahlep.Click += new System.EventHandler(this.label8sahlep_Click);
+            // 
+            // button5kahveeksi
+            // 
+            this.button5kahveeksi.BackColor = System.Drawing.Color.Gainsboro;
+            this.button5kahveeksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5kahveeksi.Location = new System.Drawing.Point(244, 126);
+            this.button5kahveeksi.Name = "button5kahveeksi";
+            this.button5kahveeksi.Size = new System.Drawing.Size(51, 42);
+            this.button5kahveeksi.TabIndex = 11;
+            this.button5kahveeksi.Text = "-";
+            this.button5kahveeksi.UseVisualStyleBackColor = false;
+            this.button5kahveeksi.Click += new System.EventHandler(this.button5kahveeksi_Click);
+            // 
+            // button6kahveartı
+            // 
+            this.button6kahveartı.BackColor = System.Drawing.Color.Silver;
+            this.button6kahveartı.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6kahveartı.Location = new System.Drawing.Point(187, 126);
+            this.button6kahveartı.Name = "button6kahveartı";
+            this.button6kahveartı.Size = new System.Drawing.Size(51, 42);
+            this.button6kahveartı.TabIndex = 10;
+            this.button6kahveartı.Text = "+";
+            this.button6kahveartı.UseVisualStyleBackColor = false;
+            this.button6kahveartı.Click += new System.EventHandler(this.button6kahveartı_Click);
+            // 
+            // label5kahves
+            // 
+            this.label5kahves.AutoSize = true;
+            this.label5kahves.Location = new System.Drawing.Point(127, 139);
+            this.label5kahves.Name = "label5kahves";
+            this.label5kahves.Size = new System.Drawing.Size(14, 16);
+            this.label5kahves.TabIndex = 9;
+            this.label5kahves.Text = "0";
+            this.label5kahves.Click += new System.EventHandler(this.label5kahves_Click);
+            // 
+            // label6kahve
+            // 
+            this.label6kahve.AutoSize = true;
+            this.label6kahve.Location = new System.Drawing.Point(43, 139);
+            this.label6kahve.Name = "label6kahve";
+            this.label6kahve.Size = new System.Drawing.Size(52, 16);
+            this.label6kahve.TabIndex = 8;
+            this.label6kahve.Text = "KAHVE";
+            this.label6kahve.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button3kolaeksi
+            // 
+            this.button3kolaeksi.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3kolaeksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3kolaeksi.Location = new System.Drawing.Point(244, 78);
+            this.button3kolaeksi.Name = "button3kolaeksi";
+            this.button3kolaeksi.Size = new System.Drawing.Size(51, 42);
+            this.button3kolaeksi.TabIndex = 7;
+            this.button3kolaeksi.Text = "-";
+            this.button3kolaeksi.UseVisualStyleBackColor = false;
+            this.button3kolaeksi.Click += new System.EventHandler(this.button3kolaeksi_Click);
+            // 
+            // button4kolaartı
+            // 
+            this.button4kolaartı.BackColor = System.Drawing.Color.Silver;
+            this.button4kolaartı.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4kolaartı.Location = new System.Drawing.Point(187, 78);
+            this.button4kolaartı.Name = "button4kolaartı";
+            this.button4kolaartı.Size = new System.Drawing.Size(51, 42);
+            this.button4kolaartı.TabIndex = 6;
+            this.button4kolaartı.Text = "+";
+            this.button4kolaartı.UseVisualStyleBackColor = false;
+            this.button4kolaartı.Click += new System.EventHandler(this.button4kolaartı_Click);
+            // 
+            // label3kolas
+            // 
+            this.label3kolas.AutoSize = true;
+            this.label3kolas.Location = new System.Drawing.Point(127, 91);
+            this.label3kolas.Name = "label3kolas";
+            this.label3kolas.Size = new System.Drawing.Size(14, 16);
+            this.label3kolas.TabIndex = 5;
+            this.label3kolas.Text = "0";
+            this.label3kolas.Click += new System.EventHandler(this.label3kolas_Click);
+            // 
+            // label4kola
+            // 
+            this.label4kola.AutoSize = true;
+            this.label4kola.Location = new System.Drawing.Point(43, 91);
+            this.label4kola.Name = "label4kola";
+            this.label4kola.Size = new System.Drawing.Size(41, 16);
+            this.label4kola.TabIndex = 4;
+            this.label4kola.Text = "KOLA";
+            this.label4kola.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button2çayeksi
+            // 
+            this.button2çayeksi.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2çayeksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2çayeksi.Location = new System.Drawing.Point(244, 30);
+            this.button2çayeksi.Name = "button2çayeksi";
+            this.button2çayeksi.Size = new System.Drawing.Size(51, 42);
+            this.button2çayeksi.TabIndex = 3;
+            this.button2çayeksi.Text = "-";
+            this.button2çayeksi.UseVisualStyleBackColor = false;
+            this.button2çayeksi.Click += new System.EventHandler(this.button2çayeksi_Click);
+            // 
+            // button1çayartı
+            // 
+            this.button1çayartı.BackColor = System.Drawing.Color.Silver;
+            this.button1çayartı.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1çayartı.Location = new System.Drawing.Point(187, 30);
+            this.button1çayartı.Name = "button1çayartı";
+            this.button1çayartı.Size = new System.Drawing.Size(51, 42);
+            this.button1çayartı.TabIndex = 2;
+            this.button1çayartı.Text = "+";
+            this.button1çayartı.UseVisualStyleBackColor = false;
+            this.button1çayartı.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2çays
+            // 
+            this.label2çays.AutoSize = true;
+            this.label2çays.Location = new System.Drawing.Point(127, 43);
+            this.label2çays.Name = "label2çays";
+            this.label2çays.Size = new System.Drawing.Size(14, 16);
+            this.label2çays.TabIndex = 1;
+            this.label2çays.Text = "0";
+            this.label2çays.Click += new System.EventHandler(this.label2çays_Click);
+            // 
+            // label1ÇAY
+            // 
+            this.label1ÇAY.AutoSize = true;
+            this.label1ÇAY.Location = new System.Drawing.Point(43, 43);
+            this.label1ÇAY.Name = "label1ÇAY";
+            this.label1ÇAY.Size = new System.Drawing.Size(34, 16);
+            this.label1ÇAY.TabIndex = 0;
+            this.label1ÇAY.Text = "ÇAY";
+            this.label1ÇAY.Click += new System.EventHandler(this.label1ÇAY_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Firebrick;
+            this.button1.Location = new System.Drawing.Point(12, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SIFIRLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Location = new System.Drawing.Point(159, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SİPARİŞİ TAMAMLA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(334, 291);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CAFE MENÜ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1çayartı;
+        private System.Windows.Forms.Label label2çays;
+        private System.Windows.Forms.Label label1ÇAY;
+        private System.Windows.Forms.Button button7sahlepeksi;
+        private System.Windows.Forms.Button button8sahlepartı;
+        private System.Windows.Forms.Label label7sahleps;
+        private System.Windows.Forms.Label label8sahlep;
+        private System.Windows.Forms.Button button5kahveeksi;
+        private System.Windows.Forms.Button button6kahveartı;
+        private System.Windows.Forms.Label label5kahves;
+        private System.Windows.Forms.Label label6kahve;
+        private System.Windows.Forms.Button button3kolaeksi;
+        private System.Windows.Forms.Button button4kolaartı;
+        private System.Windows.Forms.Label label3kolas;
+        private System.Windows.Forms.Label label4kola;
+        private System.Windows.Forms.Button button2çayeksi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cafesipariş
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int caysayisi = Convert.ToInt16(label2çays.Text);
+            caysayisi++;
+            label2çays.Text = Convert.ToString(caysayisi);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1ÇAY_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label2çays.Text = "0";
+            label3kolas.Text = "0";
+            label5kahves.Text = "0";
+            label7sahleps.Text = "0";
+            
+        }
+
+        private void label2çays_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2çayeksi_Click(object sender, EventArgs e)
+        {
+            int caysayisi = Convert.ToInt16(label2çays.Text);
+            if (caysayisi>0)
+            {
+             
+                caysayisi--;
+                label2çays.Text = Convert.ToString(caysayisi);
+            }
+
+
+        }
+
+        private void button4kolaartı_Click(object sender, EventArgs e)
+        {
+            int kolasayisi = Convert.ToInt16(label3kolas.Text);
+           
+                kolasayisi++;
+                label3kolas.Text = Convert.ToString(kolasayisi);
+            
+        }
+
+        private void button6kahveartı_Click(object sender, EventArgs e)
+        {
+            int kahvesayisi = Convert.ToInt16(label5kahves.Text);
+          
+                kahvesayisi++;
+                label5kahves.Text = Convert.ToString(kahvesayisi);
+            
+        }
+
+        private void label3kolas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3kolaeksi_Click(object sender, EventArgs e)
+        {
+            int kolasayisi = Convert.ToInt16(label3kolas.Text);
+            if (kolasayisi > 0)
+            {
+                kolasayisi--;
+                label3kolas.Text = Convert.ToString(kolasayisi);
+            }
+        }
+
+        private void label5kahves_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5kahveeksi_Click(object sender, EventArgs e)
+        {
+            int kahvesayisi = Convert.ToInt16(label5kahves.Text);
+            if ((kahvesayisi > 0))
+            {
+                kahvesayisi--;
+                label5kahves.Text = Convert.ToString(kahvesayisi);
+            }
+        }
+
+        private void label8sahlep_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8sahlepartı_Click(object sender, EventArgs e)
+        {
+            int sahlepsayisi = Convert.ToInt16(label7sahleps.Text);
+            
+                sahlepsayisi++;
+                label7sahleps.Text = Convert.ToString(sahlepsayisi);
+            
+            
+        }
+
+        private void label7sahleps_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7sahlepeksi_Click(object sender, EventArgs e)
+        {
+            int sahlepsayisi = Convert.ToInt16(label7sahleps.Text);
+            if (sahlepsayisi > 0)
+            {
+                sahlepsayisi--;
+                label7sahleps.Text = Convert.ToString(sahlepsayisi);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("SİPARİŞİNİZ :" + label2çays.Text + " ÇAY," + label3kolas.Text + " KOLA," + label5kahves.Text + " KAHVE," + label7sahleps.Text + " SAHLEP");
+        }
+    }
+}
